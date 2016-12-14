9ae4319438d1f30b02b78e5b51c624c524f1394f
Ragnar Eklund <ragnar@Ragnars-MacBook-2.local>
2016-12-14 23:51:44 +0100

    Handle dates and errors.

--- api/crud.py
+++ api/crud.py
@@ -7,5 +7,12 @@ def add(channel, message):
 	data = {"time": current_milli_time(), "message": message}
 	return storage.add(channel, data)
 
-def get(channel, fromDate=None, toDate=None):
-	return storage.get(channel)
+def get(channel, fromDate, toDate):
+	messages = storage.get(channel)
+	if messages == None:
+		return None
+	ret = []
+	for message in messages:
+		if fromDate <= message["time"] <= toDate:
+			ret.append(message)
+	return ret
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from dateutil import parser
+import time
 
 import api
 
@@ -13,21 +15,39 @@ def hello():
 @app.route("/<channel>/", methods=['GET', 'POST'])
 def channel(channel=None):
     if request.method == 'GET':
-        messages = api.get(channel)
+        fromDate = getDate(request.args["from"]) if "from" in request.args else float("-inf")
+        toDate = getDate(request.args["to"]) if "to" in request.args != None else float("inf")
+        messages = api.get(channel, fromDate, toDate)
+
         if messages == None:
-            return error(404, "Channel: ["+channel+"] not found.")
+            return send_error(404, "Channel: ["+channel+"] not found.")
         return jsonify(messages)
+
     if request.method == 'POST':
         if not request.is_json:
-            return error(406, "Use 'Content-type':'application/json'")
+            return send_error(406, "Use 'Content-type':'application/json'")
         message = request.get_json()
         api.add(channel, message)
         return ("", 201, {"Content-Type": "application/json"})
 
-def error(code, message):
+def send_error(code, message):
     respMessage = {"error": code, "message": message}
     resp = (jsonify(**respMessage), code, {"Content-Type": "application/json"})
     return resp
+
+@app.errorhandler(400)
+def bad_request(error):
+    return send_error(400, "Bad Request")
+
+@app.errorhandler(500)
+def internal_servererror(error):
+    return send_error(500, "Internal Server Error")
+
+def getDate(dateVal):
+    try:
+        return long(dateVal)
+    except ValueError:
+        return long(round(time.mktime(parser.parse(dateVal).timetuple()) * 1000))
 
 if __name__ == "__main__":
     app.run()
