94c3a64a0812d82c8c6ea2135324d7d0b68476bc
Ragnar Eklund <ragnar@Ragnars-MacBook-2.local>
2016-12-14 21:44:00 +0100

    Add project install and requirement files.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Messages',
+      version='0.1',
+      description='Simple message service',
+      url='http://github.com/reklund/messages',
+      author='Ragnar Eklund',
+      author_email='ragnar.eklund@gmail.com',
+      license='MIT',
+      packages=['messages'],
+      zip_safe=False)
